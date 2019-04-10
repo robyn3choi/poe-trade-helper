875d6253510dd2760c9cb67911acfa91e22cdf7a
robyn <robyn3choi@gmail.com>
2019-04-09 20:43:05 -0700

    initial commit. have example table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class App extends Component {
   render() {
+
+    const items = [
+      {
+        id: 0,
+        name: 'blah',
+        divCard: 'something',
+        cardsNeeded: 5,
+        pricePerCard: 100,
+        totalCardsPrice: 500,
+        itemPrice: 600,
+        profit: 100,
+        profitMargin: "10%"
+      },
+      {
+        id: 1,
+        name: 'blah',
+        divCard: 'something',
+        cardsNeeded: 5,
+        pricePerCard: 100,
+        totalCardsPrice: 500,
+        itemPrice: 600,
+        profit: 100,
+        profitMargin: "10%"
+      },
+      {
+        id: 2,
+        name: 'blah',
+        divCard: 'something',
+        cardsNeeded: 4,
+        pricePerCard: 100,
+        totalCardsPrice: 500,
+        itemPrice: 600,
+        profit: 100,
+        profitMargin: "10%"
+      },
+      {
+        id: 3,
+        name: 'blah',
+        divCard: 'something',
+        cardsNeeded: 5,
+        pricePerCard: 100,
+        totalCardsPrice: 500,
+        itemPrice: 600,
+        profit: 100,
+        profitMargin: "10%"
+      }
+    ];
+
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Div Card',
+        accessor: 'divCard'
+      },
+      {
+        Header: 'Cards Needed',
+        accessor: 'cardsNeeded'
+      },
+      {
+        Header: 'Price Per Card',
+        accessor: 'pricePerCard'
+      },
+      {
+        Header: 'Total Cards Price',
+        accessor: 'totalCardsPrice'
+      },
+      {
+        Header: 'Item Price',
+        accessor: 'itemPrice'
+      },
+      {
+        Header: 'Profit',
+        accessor: 'profit'
+      },
+      {
+        Header: 'Profit Margin',
+        accessor: 'profitMargin'
+      },
+    ];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ReactTable data={items} columns={columns} className='-striped'/>
       </div>
     );
   }
