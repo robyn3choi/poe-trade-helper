f9a9bb9779569b2a7d2dc8fd61f8f7c024196f6f
robyn <robyn3choi@gmail.com>
2019-04-16 22:26:47 -0700

    made table prettier, still need to put in button links

--- src/CardsTable/CardsTable.js
+++ src/CardsTable/CardsTable.js
@@ -16,18 +16,25 @@ class CardsTable extends React.Component {
 
   formatItems = items => {
     for (let item of items) {
-      if (!item) break; //TODO: remove
+      if (!item) continue; //TODO: remove
+
       for (let [key, val] of Object.entries(item)) {
         if (key.includes('Ex') || key === 'margin') {
-          item[key] = val.toFixed(2);
+          item[key] = +val.toFixed(2);
           if (key === 'margin') {
             item[key] += '%';
           }
         }
         else if (key.includes('Ch')) {
-          item[key] = val.toFixed(0);
+          item[key] = +val.toFixed(0);
         }
       }
+
+      item.stackPriceCh = item.cardPriceCh * item.stack;
+      item.stackPriceEx = +(item.cardPriceEx * item.stack).toFixed(2);
+      item.profitCh = item.itemPriceCh - item.stackPriceCh;
+      item.profitEx = +(item.itemPriceEx - item.stackPriceEx).toFixed(2);
+      item.margin = +(item.profitCh / item.itemPriceCh).toFixed(2);
     }
     var filtered = items.filter((item, index, arr) => item.id >= 0); //TODO: remove
     this.setState({items: filtered});
@@ -36,16 +43,25 @@ class CardsTable extends React.Component {
   render() {
     const chaosImage = <img className='cards-table__currency-icon' src={process.env.PUBLIC_URL + 'images/chaos.png'} alt='chaos' />;
     const exaltedImage = <img className='cards-table__currency-icon' src={process.env.PUBLIC_URL + 'images/exalted.png'} alt='exalted' />;
+    const searchBtn = <button className='cards-table__search-btn'>
+      <img className='cards-table__search-icon' alt='search on pathofexile.com' src='/images/search.svg' />
+      </button>;
     const columns = [
+      {
+        Header: 'Card',
+        accessor: 'card',
+        minWidth: 180,
+      },
       {
         Header: 'Item',
         accessor: 'name',
         minWidth: 140,
       },
       {
-        Header: 'Card',
-        accessor: 'card',
-        minWidth: 140,
+        Header: 'Stack',
+        accessor: 'stack',
+        minWidth: 60,
+        className: 'cards-table__stack'
       },
       {
         Header: 'Card Price',
@@ -54,13 +70,15 @@ class CardsTable extends React.Component {
             Header: props => chaosImage,
             accessor: 'cardPriceCh',
             minWidth: 60,
-            className: 'cards-table__price'
+            headerClassName: 'cards-table__ch-column',
+            className: 'cards-table__price cards-table__price_ch'
           },
           {
             Header: props => exaltedImage,
             accessor: 'cardPriceEx',
             minWidth: 60,
-            className: 'cards-table__price'
+            headerClassName: 'cards-table__ex-column',
+            className: 'cards-table__price cards-table__price_ex'
           },
         ]
       },
@@ -71,13 +89,15 @@ class CardsTable extends React.Component {
             Header: props => chaosImage,
             accessor: 'stackPriceCh',
             minWidth: 60,
-            className: 'cards-table__price'
+            headerClassName: 'cards-table__ch-column',
+            className: 'cards-table__price cards-table__price_ch'
           },
           {
             Header: props => exaltedImage,
             accessor: 'stackPriceEx',
             minWidth: 60,
-            className: 'cards-table__price'
+            headerClassName: 'cards-table__ex-column',
+            className: 'cards-table__price cards-table__price_ex'
           },
         ]
       },
@@ -87,14 +107,16 @@ class CardsTable extends React.Component {
           {
             Header: props => chaosImage,
             accessor: 'itemPriceCh',
-            minWidth: 60,
-            className: 'cards-table__price'
+            minWidth: 70,
+            headerClassName: 'cards-table__ch-column',
+            className: 'cards-table__price cards-table__price_ch'
           },
           {
             Header: props => exaltedImage,
             accessor: 'itemPriceEx',
-            minWidth: 60,
-            className: 'cards-table__price'
+            minWidth: 70,
+            headerClassName: 'cards-table__ex-column',
+            className: 'cards-table__price cards-table__price_ex'
           },
         ]
       },
@@ -104,14 +126,16 @@ class CardsTable extends React.Component {
           {
             Header: props => chaosImage,
             accessor: 'profitCh',
-            minWidth: 60,
-            className: 'cards-table__price'
+            minWidth: 70,
+            headerClassName: 'cards-table__ch-column',
+            className: 'cards-table__price cards-table__price_ch'
           },
           {
             Header: props => exaltedImage,
             accessor: 'profitEx',
-            minWidth: 60,
-            className: 'cards-table__price'
+            minWidth: 70,
+            headerClassName: 'cards-table__ex-column',
+            className: 'cards-table__price cards-table__price_ex'
           },
         ]
       },
@@ -119,26 +143,25 @@ class CardsTable extends React.Component {
         Header: 'Margin',
         accessor: 'margin',
         minWidth: 60,
+        headerClassName: 'cards-table__margin-heading',
         className: 'cards-table__margin'
       },
-    ];
-  
-    const sorted = [
       {
-        id: 'cardPriceCh',
-        desc: true
-      }
+        Header: '',
+        minWidth: 50,
+        Cell: props => searchBtn
+      },
     ];
-  
+
     return(
       <div className = "App" >
         <ReactTable data={this.state.items}
           columns={columns}
-          sorted={sorted}
+          defaultSorted={[{id:'cardPriceCh'}]}
           resizable={false}
           minRows={0}
           showPagination={false}
-          className='-striped -highlight cards-table' />
+          className='-highlight cards-table' />
       </div>
     ); 
   }
